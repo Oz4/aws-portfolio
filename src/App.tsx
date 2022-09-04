47c23a2bbc4fb18358bba32a325be3b459242870
Oz4 <aowss18@gmail.com>
2022-09-05 02:11:49 +0300

    testing subdomain

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <div className="w-full h-screen flex justify-center items-center text-7xl">
-        4ws
+        4wss
       </div>
     </div>
   )
