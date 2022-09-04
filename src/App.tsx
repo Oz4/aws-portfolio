80d1ceeef479e47d8d61d3163cd3ca7ba6f0a820
Oz4 <aowss18@gmail.com>
2022-09-05 02:28:18 +0300

    Updated: added place holder + installed framer-motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+
 function App() {
   return (
     <div className="App">
-      <div className="w-full h-screen flex justify-center items-center text-7xl">
-        4wss
+      <div className="w-full h-screen flex justify-center items-center text-7xl bg-[#fbfbfb] select-none touch-none pointer-events-none">
+        <img src="bored.gif" alt="" className="w-[400px] h-[400px]" />
+      </div>
+
+      <div className="absolute top-0 left-0 p-8 text-3xl font-semibold font-mono text-gray-600">
+        4ws
       </div>
     </div>
   )
